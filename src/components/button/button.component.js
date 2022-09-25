6e84c3e945acc86166328901bc1fe3fc8cb5634d
Jakub Widurek <jakubwidurek@gmail.com>
2022-09-25 08:05:27 +0200

    add button styled styles

--- src/components/button/button.component.js
+++ src/components/button/button.component.js
@@ -1,19 +1,25 @@
-import './button.styles.scss';
+import { ButtonInverted, ButtonBase, ButtonGoogle } from './button.styles';
 
-const BUTTON_TYPE_CLASSES = {
-  google: 'google-sign-in',
+export const BUTTON_TYPE_CLASSES = {
+  base: 'base',
+  google: 'google',
   inverted: 'inverted',
+};
+const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) => {
+  // eslint-disable-next-line default-case
+  switch (buttonType) {
+    case BUTTON_TYPE_CLASSES.base:
+      return ButtonBase;
+    case BUTTON_TYPE_CLASSES.google:
+      return ButtonGoogle;
+    case BUTTON_TYPE_CLASSES.inverted:
+      return ButtonInverted;
+  }
 };
 
 const Button = ({ children, buttonType, ...otherProps }) => {
-  return (
-    <button
-      className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
-      {...otherProps}
-    >
-      {children}
-    </button>
-  );
+  const CustomButton = getButton(buttonType);
+  return <CustomButton {...otherProps}>{children}</CustomButton>;
 };
 
 export default Button;
