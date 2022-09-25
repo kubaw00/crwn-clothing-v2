1c19d222cb592eaabd0d785f8edcae52b2003d75
Jakub Widurek <jakubwidurek@gmail.com>
2022-09-25 04:27:08 +0200

    changes product context to categories context

--- src/routes/shop/shop.component.js
+++ src/routes/shop/shop.component.js
@@ -2,12 +2,12 @@ import { useContext } from 'react';
 
 import ProductCard from '../../components/product-card/product-card.component';
 
-import { ProductsContext } from '../../contexts/products.context';
+import { CategoriesContext } from '../../contexts/categories.context';
 
 import './shop.styles.scss';
 
 const Shop = () => {
-  const { products } = useContext(ProductsContext);
+  const { products } = useContext(CategoriesContext);
 
   return (
     <div className='products-container'>
