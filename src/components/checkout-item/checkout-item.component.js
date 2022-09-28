7e121720b1f3b9b8fa32692a49b7bbb33d9205a9
Jakub Widurek <jakubwidurek@gmail.com>
2022-09-28 23:56:27 +0200

    add styles to checkout-item component

--- src/components/checkout-item/checkout-item.component.js
+++ src/components/checkout-item/checkout-item.component.js
@@ -1,8 +1,12 @@
 import { useContext } from 'react';
 
 import { CartContext } from '../../contexts/cart.context';
-
-import './checkout-item.styles.scss';
+import {
+  CheckoutItemContainer,
+  ImageContainer,
+  Quantity,
+  RemoveButton,
+} from './checkout-item.styles';
 
 const CheckoutItem = ({ cartItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
@@ -15,12 +19,12 @@ const CheckoutItem = ({ cartItem }) => {
   const removeItemHandler = () => removeItemToCart(cartItem);
 
   return (
-    <div className='checkout-item-container'>
-      <div className='image-container'>
+    <CheckoutItemContainer>
+      <ImageContainer>
         <img src={imageUrl} alt={`${name}`} />
-      </div>
+      </ImageContainer>
       <span className='name'> {name} </span>
-      <span className='quantity'>
+      <Quantity>
         <div className='arrow' onClick={removeItemHandler}>
           &#10094;
         </div>
@@ -28,12 +32,10 @@ const CheckoutItem = ({ cartItem }) => {
         <div className='arrow' onClick={addItemHandler}>
           &#10095;
         </div>
-      </span>
+      </Quantity>
       <span className='price'> {price}</span>
-      <div className='remove-button' onClick={clearItemHandler}>
-        &#10005;
-      </div>
-    </div>
+      <RemoveButton onClick={clearItemHandler}>&#10005;</RemoveButton>
+    </CheckoutItemContainer>
   );
 };
 
--- src/components/directory/directory.component.js
+++ src/components/directory/directory.component.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import DirectoryItem from '../directory-item/directory-item.component';
-import './directory.styles.scss';
+
 import { CategoriesContainer } from './directory.styles';
 
 const categories = [
